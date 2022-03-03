ccecc8098816aeac36248ef963423994e9f02f43
Santiago Dylan Terenziani <sterenziani@itba.edu.ar>
2022-03-03 16:39:22 -0300

    Added BoxPlot by sex category

--- tp0/main.py
+++ tp0/main.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from constants import Headers, Modes
 
 def print_entire_df (df):
@@ -12,7 +13,7 @@ def read_data(file):
     return df
 
 def process_replace(df):
-    return df.replace(to_replace=999.99, value='N/A')
+    return df.replace(to_replace=999.99, value=None)
 
 # Filter all the N/A rows
 # Replace all 999.99 values in the column with the calculation for that column
@@ -43,17 +44,34 @@ def process_data(df, mode):
         process_median(df)
     return df
 
+def create_boxplots(df, field):
+    men = df[df[Headers.SEXO.value] == 'M'][field]
+    women = df[df[Headers.SEXO.value] == 'F'][field]
+    data = [df[field], men, women]
+    position_values = [0, 1, 2]
+    plt.boxplot(data, positions = position_values)
+    plt.xticks(position_values[::], ["Global", "Hombres", "Mujeres"])
+    plt.title("Consumo de " +field +" por sexo")
+    plt.show()
+
 def main():
     # Parse arguments
     parser = argparse.ArgumentParser(description="Machine Learning TP0")
 
-    # add arguments
+    # Add arguments
     parser.add_argument('-f', dest='file', required=True)
     parser.add_argument('-m', dest='mode', required=True)
     args = parser.parse_args()
 
     df = read_data(args.file)
-    print_entire_df(process_data(df, args.mode))
+    new_df = process_data(df, args.mode)
+    print_entire_df(new_df)
+
+    # Create graphs
+    for header in Headers:
+        if header != Headers.SEXO:
+            h = header.value
+            create_boxplots(new_df, header.value)
 
 if __name__ == '__main__':
     main()
