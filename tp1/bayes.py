43fcf2f1976d019389076c9c52bcca4c20f8ed74
Santiago Dylan Terenziani <sterenziani@itba.edu.ar>
2022-03-22 16:27:51 -0300

    Refactor: Made Naive Bayes more generic

---
+++ tp1/bayes.py
@@ -0,0 +1,43 @@
+# Get row for specific class
+def get_df_for_class(df, header_name, _class):
+    return df[df[header_name] == _class]
+
+def compute_hmap_for_class(sample, frequencies, class_probability, header_names, class_header, class_to_calculate):
+    # Get the probabilities for just this class
+    current_class_frequencies = get_df_for_class(frequencies, class_header.value, class_to_calculate)
+    current_class_probability = get_df_for_class(class_probability, class_header.value, class_to_calculate)
+    # Start with the probability of the class itself
+    probability = current_class_probability.iloc[0][0]
+    for header in header_names:
+        # Ignore class name header (nationality / category)
+        if header != class_header:
+            # Determine if we need the probability of it being a "positive" or a "negative" example
+            # If it's a positive example, just multiply, otherwise use 1 - probability
+            if (sample[header.value][0] == 1):
+                probability *= current_class_frequencies.iloc[0][header.value]
+            else:
+                probability *= (1 - current_class_frequencies.iloc[0][header.value])
+    return probability
+
+def apply_bayes(example, frequencies, class_probability, header_names, class_header, class_names):
+    results, total, max, max_classification = {}, 0, 0, None
+    # Calculate the hmap without denominator
+    for classification in class_names:
+        # Compute the result for each nationality
+        results[classification] = compute_hmap_for_class(example, frequencies, class_probability, header_names, class_header, classification.value)
+        # Add it towards the total so that we get the correct probability
+        total += results[classification]
+    # Pretty prints
+    print("Current example to classify...")
+    print(example)
+    print('')
+    # Iterate again to properly compute the probability
+    for classification in class_names:
+        # Divide by the total
+        results[classification] = results[classification] / total
+        print("Probability of the example to be", classification.value, "is", results[classification])
+        # Check which one is the maximum one
+        if results[classification] > max:
+            max = results[classification]
+            max_classification = classification
+    print("It's most likely to be", max_classification.value)
--- tp1/exercise1.py
+++ tp1/exercise1.py
@@ -1,4 +1,5 @@
 from fileHandling import read_data
+from bayes import apply_bayes
 from constants import Ex1_Headers, Ex1_Nacionalidad
 import pandas as pd
 
@@ -7,10 +8,6 @@ def build_examples(columns):
     sample_1 = pd.DataFrame(data=[[1,0,1,1,0,'?']], columns=columns)
     sample_2 = pd.DataFrame(data=[[0,1,1,0,1,'?']], columns=columns)
     return sample_1, sample_2
-
-# Get row for specific nationality class
-def get_df_for_class(df, _class):
-    return df[df[Ex1_Headers.NACIONALIDAD.value] == _class]
 
 def compute_laplace_frequencies(df):
     # Create empty df
@@ -42,45 +39,6 @@ def compute_class_probability(df):
         _df = pd.concat([_df, nationalityProbability], ignore_index = True)
     return _df
 
-def compute_hmap_for_class(sample, frequencies, class_probability, class_to_calculate):
-    # Get the probabilities for just this class
-    current_class_frequencies, current_class_probability = get_df_for_class(frequencies, class_to_calculate), get_df_for_class(class_probability, class_to_calculate)
-    # Start with the probability of the class itself
-    probability = current_class_probability.iloc[0][0]
-    for header in Ex1_Headers:
-        # Ignore nationality header
-        if header != Ex1_Headers.NACIONALIDAD:
-            # Determine if we need the probability of it being a "positive" or a "negative" example
-            # If it's a positive example, just multiply, otherwise use 1 - probability
-            if (sample[header.value][0] == 1):
-                probability *= current_class_frequencies.iloc[0][header.value]
-            else:
-                probability *= (1 - current_class_frequencies.iloc[0][header.value])
-    return probability
-
-def apply_bayes(example, frequencies, class_probability):
-    results, total, max, max_nationality = {}, 0, 0, None
-    # Calculate the hmap without denominator
-    for nationality in Ex1_Nacionalidad:
-        # Compute the result for each nationality
-        results[nationality] = compute_hmap_for_class(example, frequencies, class_probability, nationality.value)
-        # Add it towards the total so that we get the correct probability
-        total += results[nationality]
-    # Pretty prints
-    print("Current example to classify...")
-    print(example)
-    print('')
-    # Iterate again to properly compute the probability
-    for nationality in Ex1_Nacionalidad:
-        # Divide by the total
-        results[nationality] = results[nationality] / total
-        print("Probability of the example to be", nationality.value, "is", results[nationality])
-        # Check which one is the maximum one
-        if results[nationality] > max:
-            max = results[nationality]
-            max_nationality = nationality
-    print("It's most likely to be", max_nationality.value)
-
 def run_exercise_1(file):
     # Get the data
     df = read_data(file)
@@ -90,6 +48,6 @@ def run_exercise_1(file):
     sample_1, sample_2 = build_examples(df.columns)
     # Apply Bayes to both
     print("------------------------")
-    apply_bayes(sample_1, frequencies, class_probability)
+    apply_bayes(sample_1, frequencies, class_probability, Ex1_Headers, Ex1_Headers.NACIONALIDAD, Ex1_Nacionalidad)
     print("------------------------")
-    apply_bayes(sample_2, frequencies, class_probability)
+    apply_bayes(sample_2, frequencies, class_probability, Ex1_Headers, Ex1_Headers.NACIONALIDAD, Ex1_Nacionalidad)
