549bd20f36596c5f745df851a2e2401d914225bd
FLORENCIA PETRIKOVICH <fpetrikovich@itba.edu.ar>
2022-03-24 13:02:43 -0300

    feat: ej3 all relative frecuencies calculated

--- tp1/exercise3.py
+++ tp1/exercise3.py
@@ -5,8 +5,9 @@ from constants import Ex3_Headers, Ex3_Ranks
 def run_exercise_3(file):
     df = read_csv(file)
     df = discretize_grades(df)
-    compute_rank_probability(df)
-    print_entire_df(compute_laplace_frequencies(df))
+    print_entire_df(compute_rank_probability(df))
+    print_entire_df(compute_grade_frequencies(df))
+    print_entire_df(compute_admision_frequencies(df))
 
 def discretize_grades(df):
     df[Ex3_Headers.GRE.value] = (df[Ex3_Headers.GRE.value] >= 500).astype(int)
@@ -17,18 +18,49 @@ def compute_rank_probability(df):
     return df[Ex3_Headers.RANK.value].value_counts(normalize=True,).rename_axis(Ex3_Headers.RANK.value).to_frame('probability')
 
 # Probability of a grade given a rank P(Grade|Rank)
-def compute_laplace_frequencies(df):
+def compute_grade_frequencies(df):
     # Create empty df
     _df = pd.DataFrame()
     for rank in Ex3_Ranks:
         # Get df filter for given rank
-        filteredDf = df[df[Ex3_Headers.RANK.value] == rank.value][[Ex3_Headers.GRE.value, Ex3_Headers.GPA.value]]
+        filtered_df = df[df[Ex3_Headers.RANK.value] == rank.value][[Ex3_Headers.GRE.value, Ex3_Headers.GPA.value]]
         # Compute sum across columns
-        laplaceDf = pd.DataFrame(data=filteredDf.sum(axis = 0, numeric_only = True)).transpose()
+        laplace_df = pd.DataFrame(data=filtered_df.sum(axis = 0, numeric_only = True)).transpose()
         # Apply Laplace to the frequencies given the current class
-        laplaceDf = (laplaceDf + 1) / (filteredDf.shape[0] + len(Ex3_Ranks))
+        laplace_df = (laplace_df + 1) / (filtered_df.shape[0] + len(Ex3_Ranks))
+        # Append the negated probabilities
+        laplace_df['no_'+Ex3_Headers.GRE.value] = laplace_df.apply(lambda laplace_df: compute_negation_probability(laplace_df, Ex3_Headers.GRE), axis=1)
+        laplace_df['no_'+Ex3_Headers.GPA.value] = laplace_df.apply(lambda laplace_df: compute_negation_probability(laplace_df, Ex3_Headers.GPA), axis=1)
         # Append the rank
-        laplaceDf[Ex3_Headers.RANK.value] = rank.value
+        laplace_df[Ex3_Headers.RANK.value] = rank.value
         # Concat the new class probabilities
-        _df = pd.concat([_df, laplaceDf], ignore_index = True)
+        _df = pd.concat([_df, laplace_df], ignore_index = True)
     return _df
+
+# Probability of a admision given a rank, gpa, and gre
+def compute_admision_frequencies(df):
+    grade_combinations = [[0, 0], [0, 1], [1, 0], [1,1]]
+    # Create empty df
+    _df = pd.DataFrame()
+    for rank in Ex3_Ranks:
+        for grades in grade_combinations:
+            # Get df filter for given rank
+            filtered_df = df[(df[Ex3_Headers.RANK.value] == rank.value) & (df[Ex3_Headers.GRE.value] == grades[0]) & (df[Ex3_Headers.GPA.value] == grades[1])]
+            # Take only the column we want to analyze
+            filtered_df = filtered_df[[Ex3_Headers.ADMIT.value]]
+            # Compute sum across columns
+            laplace_df = pd.DataFrame(data=filtered_df.sum(axis = 0)).transpose()
+            # Apply Laplace to the frequencies given the current class
+            laplace_df = (laplace_df + 1) / (filtered_df.shape[0] + (len(Ex3_Ranks)*len(grade_combinations)))
+            # Append the probability of no admition
+            laplace_df['no_'+Ex3_Headers.ADMIT.value] = laplace_df.apply(lambda laplace_df: compute_negation_probability(laplace_df, Ex3_Headers.ADMIT), axis=1)
+            # Append the fixed values
+            laplace_df[Ex3_Headers.RANK.value] = rank.value
+            laplace_df[Ex3_Headers.GRE.value] = grades[0]
+            laplace_df[Ex3_Headers.GPA.value] = grades[1]
+            # Concat the new class probabilities
+            _df = pd.concat([_df, laplace_df], ignore_index = True)
+    return _df
+
+def compute_negation_probability(df, column_header):
+    return 1 - df[column_header.value]
