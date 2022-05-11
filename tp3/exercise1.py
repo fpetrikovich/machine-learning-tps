7a3f14ac1d098f8c931277c26315e9ecdba8ff82
sterenziani <sterenziani@itba.edu.ar>
2022-05-11 17:26:02 -0300

    Feat: Added optimal hyperplane

--- tp3/exercise1.py
+++ tp3/exercise1.py
@@ -9,7 +9,9 @@ import datetime
 import math
 import random
 
-def generate_points(amount, margin, misclassifications, a,b,c):
+def generate_points(amount, margin, misclassifications, a,b,c, seed=None):
+    if seed:
+        np.random.seed(seed)
     points = np.random.random((amount,3)) * 5
     for p in points:
         f = a*p[0] + b*p[1] + c
@@ -36,26 +38,32 @@ def get_hyperplane_vector(x,y):
     x = list(x)
     a = (y[-1]-y[0])/(x[-1]-x[0])
     b = -1
-    c = y[x.index(0)]
-
-    v = [a,b,c]
-    normalized_v = v/np.linalg.norm(v)
-    return normalized_v[0], normalized_v[1], normalized_v[2]
+    c = -a*x[-1] - b*y[-1]
+    return [a,b,c]
 
 def run_exercise_1():
     # Usamos la recta "y = x"
     x = np.linspace(0, 5, 10)
     y = x
     a,b,c = get_hyperplane_vector(x,y)
-    points = generate_points(25, 0.5, 0, a,b,c)
+    points = generate_points(25, 0.25, 0, a,b,c)
     perceptron = SimplePerceptron()
-    w = perceptron.algorithm(points) # w = [b0, b1, b2] = [c, a, b]
+
+    w, margin = perceptron.algorithm(points) # w = [b0, b1, b2] = [c, a, b]
     y_perceptron = -w[1]/w[2] * x - w[0]/w[2]
-    # TODO: Buscar hiperplano óptimo
-    # TODO: Usar SVM para clasificar
+    optimal_vector, optimal_margin, optimal_points, optimal_dist_y = perceptron.optimal_hiperplane(w, points, 4)
+    y_optimal = -optimal_vector[1]/optimal_vector[2] * x - optimal_vector[0]/optimal_vector[2]
+
+    print("Perceptron margin:", margin, "\nPerceptron Weights: ", w)
+    print("Optimal margin:", optimal_margin, "\nOptimal Weights: ", optimal_vector)
     plt.scatter(points[:,0], points[:,1], c=points[:,2])
-    plt.plot(x, y, color='blue')
-    plt.plot(x, y_perceptron, color='red')
+    #plt.plot(x, y, color='grey', linestyle='dotted', alpha=0.25)
+    plt.plot(x, y_perceptron, color='red', label='Perceptron')
+    plt.plot(x, y_optimal, color='green', label='Óptimo')
+    plt.plot(x, y_optimal+optimal_dist_y/2, color='green', linestyle='dotted', alpha=0.5)
+    plt.plot(x, y_optimal-optimal_dist_y/2, color='green', linestyle='dotted', alpha=0.5)
     plt.xlim(-0.1, 5.1)
     plt.ylim(-0.1, 5.1)
+    plt.legend()
     plt.show()
+    # TODO: Usar SVM para clasificar
--- tp3/perceptron.py
+++ tp3/perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import math
 from datetime import datetime
 
 class SimplePerceptron:
@@ -41,6 +42,23 @@ class SimplePerceptron:
     def get_activation(self, sumatoria):
         return self.step(sumatoria)
 
+    def is_valid_classifier(self, data, margin, a,b,c):
+        for p in data:
+            if (a*p[0] + b*p[1] + c)*p[2] < 0:
+                return False
+            if self.calculate_distance(p, a,b,c) < margin:
+                return False
+        return True
+
+    def calculate_distance(self, p, a, b, c):
+        return abs(a*p[0] + b*p[1] + c) / math.sqrt(a**2 + b**2)
+
+    def calculate_margin(self, data, a,b,c):
+        distances = []
+        for p in data:
+            distances.append(self.calculate_distance(p,a,b,c))
+        return min(distances)
+
     def algorithm(self, input):
         data = []
         for p in input:
@@ -71,7 +89,55 @@ class SimplePerceptron:
                     w_min = weights
             else:
                 break
-        return w_min
+        w_min = w_min[:,0]
+        plane_margin = self.calculate_margin(input, w_min[1], w_min[2], w_min[0])
+        return w_min, plane_margin
+
+    def optimal_hiperplane(self, weights, data, n=4):
+        data = list(data)
+        data.sort(key=lambda t: self.calculate_distance(t, weights[1], weights[2], weights[0]))
+        class1 = []
+        class2 = []
+        for p in data:
+            if p[2] == 1 and len(class1) < n:
+                class1.append(p)
+            if p[2] == -1 and len(class2) < n:
+                class2.append(p)
+
+        best_margin = -1
+        best_vector = None
+        best_points = None
+        best_dist_y = None
+
+        for dual_class in [class1, class2]:
+            single_class = class1
+            if dual_class is single_class:
+                single_class = class2
+            for single_point in single_class:
+                for i in range(len(dual_class)):
+                    for j in range(i+1, len(dual_class)):
+                        single_point = single_point
+                        dual_p1 = dual_class[i]
+                        dual_p2 = dual_class[j]
+                        # Hiperplano generado por dos puntos:
+                        a = (dual_p2[1]-dual_p1[1])/(dual_p2[0]-dual_p1[0])
+                        b = -1
+                        c = dual_p2[1] - a*dual_p2[0]
+                        # Buscamos paralela que atraviese punto singular y buscamos la recta entre esas dos
+                        dist_y = single_point[1] - (a*single_point[0]+c)
+                        c += dist_y/2
+                        # Si este hiperplano clasifica bien y es maximal, lo guardamos
+                        dist_dual_1 = self.calculate_distance(dual_p1, a,b,c)
+                        dist_dual_2 = self.calculate_distance(dual_p2, a,b,c)
+                        dist_single = self.calculate_distance(single_point, a,b,c)
+                        if self.is_valid_classifier(data, min([dist_dual_1, dist_dual_2, dist_single]), a,b,c):
+                            plane_margin = self.calculate_margin(data, a,b,c)
+                            if plane_margin > best_margin:
+                                best_margin = plane_margin
+                                best_vector = [c,a,b]
+                                best_points = np.array([single_point, dual_p1, dual_p2])
+                                best_dist_y = dist_y
+        return best_vector, best_margin, best_points, best_dist_y
 
     def test_perceptron(self, test_data, weights):
         print('Testing perceptron...')
