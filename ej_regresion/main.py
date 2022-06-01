d9310410048981b3bd7b28f84a2b3a3d38fea261
FLORENCIA PETRIKOVICH <fpetrikovich@itba.edu.ar>
2022-06-01 17:25:39 -0300

    feat: added headers

--- ej_regresion/main.py
+++ ej_regresion/main.py
@@ -2,7 +2,7 @@ import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from constants import Headers, Modes
+from constants import Headers
 import matplotlib.gridspec as gridspec
 
 def print_entire_df (df):
@@ -10,59 +10,20 @@ def print_entire_df (df):
     print(df)
 
 def read_data(file):
-    df = pd.read_excel(file)
-    return df
-
-def process_replace(df):
-    return df.replace(to_replace=999.99, value=np.nan)
-
-# Filter all the N/A rows
-# Replace all 999.99 values in the column with the calculation for that column
-def process_mean(df):
-    for header in Headers:
-        # Not process Sexo
-        if header != Headers.SEXO:
-            h = header.value
-            filter = df[h] != 999.99
-            df[h] = df[h].replace(to_replace=999.99, value=np.mean(df[filter][h]))
-    return df
-
-def process_median(df):
-    for header in Headers:
-        if header != Headers.SEXO:
-            h = header.value
-            filter = df[h] != 999.99
-            df[h] = df[h].replace(to_replace=999.99, value=np.median(df[filter][h]))
-    return df
-
-# Processing changes according to given mode
-def process_data(df, mode):
-    if mode == Modes.REMOVE.name:
-        df = process_replace(df)
-    elif mode == Modes.MEDIAN.name:
-        df = process_mean(df)
-    elif mode == Modes.MEAN.name:
-        df = process_median(df)
+    df = pd.read_csv(file)
     return df
 
 
 def main():
     # Parse arguments
-    parser = argparse.ArgumentParser(description="Machine Learning TP0")
+    parser = argparse.ArgumentParser(description="Machine Learning Regression Exercise")
 
     # Add arguments
     parser.add_argument('-f', dest='file', required=True)
-    parser.add_argument('-m', dest='mode', required=True)
     args = parser.parse_args()
 
     df = read_data(args.file)
-    new_df = process_data(df, args.mode)
-    print_entire_df(new_df)
-
-    # Create BoxPlot and Scatterplot graphs
-    for header in Headers:
-        if header != Headers.SEXO:
-            h = header.value
+    print_entire_df(df)
 
 
 if __name__ == '__main__':
